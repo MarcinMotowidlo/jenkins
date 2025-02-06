15686e7d4f1d320220f4b3c9107553f19fc85758
Marcin Motowidlo <marcin.motowidlo@gmail.com>
2025-02-06 18:56:53 +0100

    no-issue: synch with repo

--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -1,17 +1,13 @@
 package com.example.jenkins;
 
-import com.example.jenkins.base.IntegrationTest;
-import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Category(IntegrationTest.class)
 class JenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("Maybe not pretty but some INTEGRATION logs");
 	}
 
 }
